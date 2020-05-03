9ee4baaef4c3af31df1282ce362c19f9d4cccc65
Sadosco <sadoscolima@gmail.com>
2020-05-03 20:04:36 -0300

    Insert provider with store

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './config/ReactotronConfig';
+import { Provider } from 'react-redux';
 
 import { BrowserRouter } from 'react-router-dom';
 
+import store from './store';
 import Routes from './routes';
 
 import GlobalStyles from './styles/global';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-      <GlobalStyles />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes />
+        <GlobalStyles />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
