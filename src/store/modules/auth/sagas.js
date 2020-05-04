2a38d7673d463cf8ace15df67e54f827a5732ec9
Gabriel Rodrigues <gbo10rodrigues@gmail.com>
2020-05-03 22:11:06 -0300

    Fix token variable

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -3,18 +3,16 @@ import { all, call, takeLatest } from 'redux-saga/effects';
 import api from '../../../services/api';
 
 function* signInRequest({ email, password }) {
-  console.tron.log('testeee');
+  const response = yield call(api.get, 'session');
 
-  const response = yield call(api.get, `session`);
-
-  const { token } = response.data.find(
+  const user = response.data.find(
     ({ email: userEmail, password: userPassword }) =>
       userEmail === email && userPassword === password
   );
 
-  if (!token) return;
+  if (!user) return;
 
-  console.tron.log(token);
+  console.tron.log(user.token);
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signInRequest)]);
