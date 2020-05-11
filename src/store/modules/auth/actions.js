131cb9c1ea96df246620e6a18ff339a1e0ff55b4
Sadosco <sadoscolima@gmail.com>
2020-05-10 23:15:09 -0300

    Auth actions

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -5,3 +5,16 @@ export function signInRequest(email, password) {
     password,
   };
 }
+
+export function signInSuccess(token, user) {
+  return {
+    type: '@auth/SIGN_IN_SUCCESS',
+    payload: { token, user },
+  };
+}
+
+export function signFailure() {
+  return {
+    type: '@auth/SIGN_IN_FAILURE',
+  };
+}
