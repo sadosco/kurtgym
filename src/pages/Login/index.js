eb309eb76f69f3d959e0ed3a9ef2de3552702882
Sadosco <sadoscolima@gmail.com>
2020-05-10 23:12:22 -0300

    Loading session app

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import * as Auth from '../../store/modules/auth/actions';
 import logo from '../../assets/images/kurtgym-logo2.png';
 
 export default function Login() {
   const dispatch = useDispatch();
+  const loading = useSelector((state) => state.auth.loading);
 
   function handleSubmit({ email, password }) {
     dispatch(Auth.signInRequest(email, password));
@@ -21,7 +22,9 @@ export default function Login() {
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
 
-        <button type="submit">Acessar conta</button>
+        <button type="submit">
+          {loading ? 'Carregando...' : 'Acessar conta'}
+        </button>
         <Link to="/signup">Criar conta</Link>
       </Form>
     </>
