76d3c0c9c3d5ff4bcf821fd68cea3171e0abdc84
Sadosco <sadoscolima@gmail.com>
2020-05-10 23:12:53 -0300

    Get logged state

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -5,12 +5,14 @@ import PropTypes from 'prop-types';
 import AuthLayout from '../pages/_layouts/auth';
 import DefaultLayout from '../pages/_layouts/default';
 
+import { store } from '../store';
+
 export default function RoutesWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const logged = false;
+  const { logged } = store.getState().auth;
 
   if (!logged && isPrivate) {
     return <Redirect to="/" />;
